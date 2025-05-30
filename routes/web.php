d433aaf86dc848756035bfd876130d4cc286acf9
hanacoric <hancor01@easv365.dk>
2025-05-31 01:27:22 +0200

    migrations route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 
 Route::get('/', function () {
     return response('Laravel is running!');
+});
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations run successfully';
 });
