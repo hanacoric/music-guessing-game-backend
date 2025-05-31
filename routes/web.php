99303e6763275292cbec26a17585ca12fab49356
hanacoric <hancor01@easv365.dk>
2025-05-31 03:30:07 +0200

    fixes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,3 @@ use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return response('Laravel is running!');
 });
-
-Route::get('/run-migrations', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return 'Migrations run successfully';
-});
